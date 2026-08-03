aead66d23d49c364ad146ef2e5a94f38a3fb27b3
shreeharsha <shreeharsha0125@gmail.com>
2026-08-03 14:57:58 +0530

    Time: 7 ms (55.52%), Space: 13.6 MB (49.54%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,14 +2,16 @@ class Solution():
     def moveZeroes(self, nums):
         w = 0
         n = len(nums)
+        
         for i in range(n):
             if nums[i] != 0:
                 nums[w] = nums[i]
                 w += 1
+        
         while w < n:
             nums[w]  = 0          
             w += 1
-           
+
         return nums
 
 nums = [0,1,0,3,12]

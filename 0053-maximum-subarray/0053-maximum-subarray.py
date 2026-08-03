4935239b1ab6e066bf65aa9d75228ea9235623e6
shreeharsha <shreeharsha0125@gmail.com>
2026-08-03 14:27:43 +0530

    Time: 97 ms (40.25%), Space: 21.1 MB (70.89%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,6 +4,7 @@ class Solution:
         max_sum = nums[0]
         for i in range(1, len(nums)):
             current_sum = max(nums[i], current_sum + nums[i])
+            
             max_sum = max(max_sum, current_sum)
         return max_sum
 nums = [-2,1,-3,4,-1,2,1,-5,4]

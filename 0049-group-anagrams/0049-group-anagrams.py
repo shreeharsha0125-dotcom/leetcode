5b882f0e0422e769c529102dd80da7d73f9c2d46
shreeharsha <shreeharsha0125@gmail.com>
2026-08-28 10:28:16 +0530

    Time: 131 ms (5.02%), Space: 16.2 MB (53.87%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        freq = {}
+        
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            
+            if sorted_word not in freq:
+                freq[sorted_word] = []
+                
+            freq[sorted_word].append(word)
+        print(freq)
+            
+        return list(freq.values())
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+obj = Solution()
+print(obj.groupAnagrams(strs))
+    
+strs = ["eat","tea","tan","ate","nat","bat"]
+obj = Solution()
+print(obj.groupAnagrams(strs))
+        
+            
+
+        

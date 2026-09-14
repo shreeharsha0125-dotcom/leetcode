39c9bf3209a6b99bc34124761df538d4ba8b39cd
shreeharsha <shreeharsha0125@gmail.com>
2026-09-14 10:17:53 +0530

    Time: 46 ms (38.26%), Space: 14 MB (25.09%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def replaceElements(self, arr):
-        ans = [-1]*len(arr)
-        for i in range(len(arr)-1,0,-1):
-            ans[i-1] = max(arr[i] , ans[i])
-        return ans
-arr = [17,18,5,4,6,1]
+        max_so_far = -1
+        for i in range(len(arr)-1, -1, -1):
+            temp = arr[i]
+            arr[i] = max_so_far
+            max_so_far = max(max_so_far, temp)
+        return arr
 
+arr = [17,18,5,4,6,1]
 obj = Solution()
-print(obj.replaceElements(arr))
+print(obj.replaceElements(arr)) 
